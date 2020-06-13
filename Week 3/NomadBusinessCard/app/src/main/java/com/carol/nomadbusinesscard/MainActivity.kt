dbd10eafb978b5bc51ff4e2d9aa5dfff27ae7d37
carolinemusyoka <carolmusyoka127@gmail.com>
2020-06-13 16:02:10 +0300

    Step 1
    1. Creating the necessary components in the activity_main.xml
    2. Adding functionality  to the button and texts
    3. Adding resources to the string.xml folder

--- Week 3/NomadBusinessCard/app/src/main/java/com/carol/nomadbusinesscard/MainActivity.kt
+++ Week 3/NomadBusinessCard/app/src/main/java/com/carol/nomadbusinesscard/MainActivity.kt
@@ -1,12 +1,63 @@
 package com.carol.nomadbusinesscard
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var text: TextView
+    private lateinit var mybtn: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+        val callme = findViewById<View>(R.id.callme)
+        callme.setOnClickListener {
+            val callIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:+254741102889"))
+            startActivity(callIntent)
+
+            Handler().postDelayed({
+                Toast.makeText(
+                    this,
+                    "Please leave a message if I am not available",
+                    Toast.LENGTH_LONG
+                ).show()
+            }, 4000)
+        }
+
+        val emailme = findViewById<View>(R.id.emailme)
+        emailme.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:thenomadke@gmail.com") // only email apps should handle this
+            intent.putExtra(Intent.EXTRA_EMAIL, "")
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback")
+            startActivity(intent)
+
+        }
+
+        text = findViewById(R.id.quotespace)
+        mybtn = findViewById(R.id.quotegenerator)
+        val facts = arrayOf(
+            "Kenya is ....",
+            "sdjasjdnksjd",
+            "snsankxasmkas",
+            "sxjksaxkdasmxds"
+        )
+        mybtn.setOnClickListener {
+            val randomQuote = facts.random()
+            text.setText(randomQuote)
+
+
+        }
+
     }
 }
