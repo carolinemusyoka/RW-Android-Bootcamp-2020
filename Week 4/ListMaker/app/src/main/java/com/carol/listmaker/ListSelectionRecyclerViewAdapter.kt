d836212764b2b55440cf27b505ec12f97cb098af
carolinemusyoka <carolmusyoka127@gmail.com>
2020-06-17 21:29:26 +0300

    Filling the blanks
    Creating an array to be passed and adding a return value to getItemCount()

--- Week 4/ListMaker/app/src/main/java/com/carol/listmaker/ListSelectionRecyclerViewAdapter.kt
+++ Week 4/ListMaker/app/src/main/java/com/carol/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -1,6 +1,20 @@
 package com.carol.listmaker
 
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListSelectionRecyclerViewAdapter: RecyclerView.Adapter<ListSelectionViewHolder>() {
+    val listTitles = arrayOf("Shopping List", "Chores", "Android","Tutorials")
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        return listTitles.size
+       
+    }
+
+    override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
