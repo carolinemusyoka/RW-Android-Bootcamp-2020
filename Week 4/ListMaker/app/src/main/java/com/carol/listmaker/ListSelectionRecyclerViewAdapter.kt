21bc3ce007336aa721d64a488f700996cbf218f9
carolinemusyoka <carolmusyoka127@gmail.com>
2020-06-17 21:39:28 +0300

    Creating ViewHolder

--- Week 4/ListMaker/app/src/main/java/com/carol/listmaker/ListSelectionRecyclerViewAdapter.kt
+++ Week 4/ListMaker/app/src/main/java/com/carol/listmaker/ListSelectionRecyclerViewAdapter.kt
@@ -1,17 +1,20 @@
 package com.carol.listmaker
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListSelectionRecyclerViewAdapter: RecyclerView.Adapter<ListSelectionViewHolder>() {
     val listTitles = arrayOf("Shopping List", "Chores", "Android","Tutorials")
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+         val view = LayoutInflater.from(parent.context)
+             .inflate(R.layout.list_selection_view_holder,parent,false)
+        return ListSelectionViewHolder()
     }
 
     override fun getItemCount(): Int {
         return listTitles.size
-       
+
     }
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
