ec480e9ab753f607e4bfaa2b7dc92828d506cbb3
carolinemusyoka <carolmusyoka127@gmail.com>
2020-06-17 20:48:13 +0300

    Initial Commit for ListMaker

---
+++ Week 4/ListMaker/app/src/main/java/com/carol/listmaker/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.carol.listmaker
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+import org.intellij.lang.annotations.JdkConstants
+
+class MainActivity : AppCompatActivity() {
+ lateinit var listRecyclerView: RecyclerView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+        listRecyclerView = findViewById(R.id.list_recyclerview)
+        listRecyclerView.layoutManager = LinearLayoutManager(this)
+        listRecyclerView.adapter = ListSelectionRecyclerViewAdapter()
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
