0e75f06b26e2e5cc4b44bf96186a8745a69ca25f
carolinemusyoka <carolmusyoka127@gmail.com>
2020-06-18 13:22:53 +0300

    Adapting the Adapter

--- Week 4/ListMaker/app/src/main/java/com/carol/listmaker/ListItemViewHolder.kt
+++ Week 4/ListMaker/app/src/main/java/com/carol/listmaker/ListItemViewHolder.kt
@@ -1,6 +1,7 @@
 package com.carol.listmaker
 
 import android.view.View
+import androidx.recyclerview.widget.RecyclerView
 
 class ListItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 }
--- Week 4/ListMaker/app/src/main/java/com/carol/listmaker/ListItemsRecyclerViewAdapter.kt
+++ Week 4/ListMaker/app/src/main/java/com/carol/listmaker/ListItemsRecyclerViewAdapter.kt
@@ -1,6 +1,21 @@
 package com.carol.listmaker
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class ListItemsRecyclerViewAdapter(var list: TaskList): RecyclerView.Adapter<ListItemViewHolder> {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListItemViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.task_view_holder, parent, false)
+        return ListItemViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return list.tasks.size
+    }
+
+    override fun onBindViewHolder(holder: ListItemViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
 }
