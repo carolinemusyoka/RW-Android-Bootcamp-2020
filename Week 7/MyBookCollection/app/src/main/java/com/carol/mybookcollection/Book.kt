6c8347618943e3e0151876399672cef1ee23e891
carolinemusyoka <carolmusyoka127@gmail.com>
2020-07-11 21:53:20 +0300

    ignore

---
+++ Week 7/MyBookCollection/app/src/main/java/com/carol/mybookcollection/Book.kt
@@ -0,0 +1,8 @@
+package com.carol.mybookcollection
+
+class Book(name: String, description: String, image: Int) {
+    var name: String? = name
+    var description: String? = description
+    var image: Int? = image
+
+}
